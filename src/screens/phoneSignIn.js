f6beeb1053a07bc0556e987c3766703b46f254e6
jdebruin <joeydebruin@gmail.com>
2020-04-14 13:40:27 -0700

    Initial styles for phoneSignIn, graphic not working yet

--- src/screens/phoneSignIn.js
+++ src/screens/phoneSignIn.js
@@ -8,6 +8,7 @@ import {
   View,
   Text,
   SafeAreaView,
+  Image
 } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import {useNavigation, useNavigationParam} from 'react-navigation-hooks';
@@ -52,16 +53,29 @@ const PhoneSignIn = () => {
     <SafeAreaView style={styles.safeView}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
         <View style={styles.container}>
-          <Button title="Phone Number Sign In" onPress={handleSubmit} />
-          <TextInput
+          <View>
+            <Text style={styles.logo}>Let's</Text>
+            <Text style={styles.subtitle}>Get started!</Text>
+          </View>
+          <Image style ={styles.graphic} source={require('../images/smartphone 1.png')}></Image>
+          <View style={styles.verfificationBox}>
+            <View style={styles.title}></View>
+            <TextInput
             value={phoneNumber}
             onChangeText={handleNumberChange}
             style={styles.textInput}
             keyboardType="phone-pad"
             maxLength={15}
             placeholder="+1 650-555-1234"
-          />
-          {error ? <Text>{error}</Text> : null}
+            placeholderTextColor={'#8D8D8D'}
+            />
+            <TouchableWithoutFeedback onPress={handleSubmit}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>Send Code</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            {error ? <Text>{error}</Text> : null}
+          </View>
         </View>
       </TouchableWithoutFeedback>
     </SafeAreaView>
@@ -71,19 +85,59 @@ const PhoneSignIn = () => {
 const styles = StyleSheet.create({
   safeView: {
     flex: 1,
+    backgroundColor: '#FFFFFF'
+  },
+  logo: {
+    fontFamily : 'Trebuchet MS',
+    paddingBottom: 10,
+    fontSize: 58,
+    textAlign: 'center'
+  },
+  subtitle: {
+    fontFamily: 'AppleSDGothicNeo-Regular',
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#8D8D8D'
   },
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
   },
+  verfificationBox: {
+    alignItems: 'center'
+  },
   textInput: {
-    height: 20,
-    width: 200,
-    borderColor: 'black',
+    fontSize: 13,
+    height: 36,
+    width: 262,
     borderWidth: 1,
     color: 'black',
+    borderColor: '#F1F3F6',
+    borderRadius: 25,
+    backgroundColor: '#F1F3F6',
+    paddingLeft: 20
   },
+  button: {
+    height: 53,
+    width: 150,
+    backgroundColor: '#0066FF',
+    color: 'white',
+    borderRadius: 5,
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: { 
+    fontSize: 16,
+    fontFamily: 'AppleSDGothicNeo-Regular',
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  graphic: {
+    height: 200,
+    width: 200
+  }
 });
 
 export default PhoneSignIn;
