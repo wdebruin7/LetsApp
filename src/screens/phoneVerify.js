14866f4dffe2284ba2b2bf7d319e18bfefe9ce82
JoeyDeBruin <joeydebruin@gmail.com>
2020-04-15 08:10:48 -0700

    Style onboarding screens (#7)
    * updated cocoapods version in pod install
    
    * Initial styles for phoneSignIn, graphic not working yet
    
    * finished initial style of phoneSignIn screen
    
    * remove graphic on text focus
    
    * Add 'verify your phone' text
    
    * styled phoneVerify screen
    
    * styled phoneVerify and simplified focus handler

--- src/screens/phoneVerify.js
+++ src/screens/phoneVerify.js
@@ -5,6 +5,8 @@ import {
   TouchableWithoutFeedback,
   View,
   Button,
+  Text,
+  Image,
   TextInput,
   Keyboard,
 } from 'react-native';
@@ -15,7 +17,10 @@ import {useSession} from '../firebase/auth';
 const PhoneVerify = ({route}) => {
   const [confirmtionCode, setConfirmationCode] = useState('');
   const [confirm, setConfirm] = useState(null);
+  const [textFocus, changeFocus] = useState(false); 
+
   const session = useSession();
+
 
   const {navigate} = useNavigation();
   const phoneNumber = useNavigationParam('phoneNumber');
@@ -50,14 +55,31 @@ const PhoneVerify = ({route}) => {
     <SafeAreaView style={styles.safeView}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
         <View style={styles.container}>
-          <Button title="Confirm Code" onPress={() => confirmCode()} />
-          <TextInput
+          <View style={styles.header}>
+            <Text style={styles.logo}>Let's</Text>
+            <Text style={styles.subtitle}>Get verified!</Text>
+          </View>
+          <Image style ={!textFocus ? styles.graphic : styles.graphic_hidden} source={require('../images/mailbox.png')}></Image>
+          <View style={styles.verfificationBox}>
+            <Text style={styles.title}>Verification code</Text>
+            <Text>Enter the code sent to</Text>
+            <Text style={{color: '#0066FF'}}>{phoneNumber}</Text>
+            <TextInput
             value={confirmtionCode}
             onChangeText={setConfirmationCode}
             style={styles.textInput}
             placeholder="123456"
             keyboardType="number-pad"
-          />
+            placeholderTextColor={'#8D8D8D'}
+            onFocus={() => changeFocus(true)}
+            onBlur={() => changeFocus(false)}
+            />
+            <TouchableWithoutFeedback onPress={() => confirmCode()}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>Verify</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
         </View>
       </TouchableWithoutFeedback>
     </SafeAreaView>
@@ -65,20 +87,77 @@ const PhoneVerify = ({route}) => {
 };
 
 const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    paddingTop: 50,
+    paddingBottom: 25
+  },
   safeView: {
     flex: 1,
+    backgroundColor: '#FFFFFF'
+  },
+  logo: {
+    fontFamily : 'Trebuchet MS',
+    paddingBottom: 10,
+    fontSize: 58,
+    textAlign: 'center'
+  },
+  subtitle: {
+    fontFamily: 'AppleSDGothicNeo-Regular',
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#8D8D8D'
   },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  verfificationBox: {
+    alignItems: 'center',
+    flex: 3,
+    margin: 20
+  },
   textInput: {
-    height: 20,
-    width: 200,
-    borderColor: 'black',
+    fontSize: 13,
+    height: 36,
+    width: 262,
     borderWidth: 1,
     color: 'black',
+    borderColor: '#F1F3F6',
+    borderRadius: 25,
+    backgroundColor: '#F1F3F6',
+    paddingLeft: 20,
+    margin: 20
   },
+  title: {
+    fontSize: 18,
+    fontFamily: 'AppleSDGothicNeo-Regular',
+    color: '#0066FF',
+    fontWeight: 'bold'
+  },
+  button: {
+    height: 53,
+    width: 150,
+    backgroundColor: '#0066FF',
+    color: 'white',
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: { 
+    fontSize: 16,
+    fontFamily: 'AppleSDGothicNeo-Regular',
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  graphic: {
+    height: 200,
+    width: 200
+  },
+  graphic_hidden: {
+    display: 'none'
+  }
 });
+
 export default PhoneVerify;
