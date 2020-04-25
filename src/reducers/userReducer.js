1a0c50f9eff87327f6201e380a5f22d6512dd618
William DeBruin <will.debruin7@gmail.com>
2020-04-25 14:02:25 -0400

    session.userData === null on set session.user, remove session.user

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,14 +1,26 @@
+import auth from '@react-native-firebase/auth';
 import userTypes from '../actions/userTypes';
 
-export default function userReducer(state, action) {
+const initialState = () => {
+  const user = auth().currentUser;
+  return {
+    initializing: !user,
+    user,
+    userData: null,
+  };
+};
+
+const userReducer = (state, action) => {
   switch (action.type) {
     case userTypes.SET:
-      return {user: action.payload, initializing: false, userData: {}};
+      return {user: action.payload, initializing: false, userData: null};
     case userTypes.REMOVE:
-      return {user: null, initializing: false, userData: {}};
+      return {user: null, initializing: false, userData: null};
     case userTypes.UPDATE:
       return {...state, initializing: false, userData: action.payload};
     default:
       return state;
   }
-}
+};
+
+export {initialState, userReducer};
