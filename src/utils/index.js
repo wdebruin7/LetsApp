2d7089cccb2b79b9e61256009142deac23005da0
wdebruin7 <will.debruin7@gmail.com>
2020-04-30 14:05:41 -0400

    created getDownloadURL util (#27)

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,3 @@
 export {default as normalizePhoneNumber} from './normalizePhoneNumber';
 export {default as validatePhoneNumber} from './validatePhoneNumber';
+export {default as getDownloadURL} from './getDownloadURL';
