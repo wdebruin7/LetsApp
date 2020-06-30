d55283b8692552d063b0ba5afdfdd94f18152daa
William DeBruin <will.debruin7@gmail.com>
2020-06-30 11:46:50 -0600

    wip

--- src/components/activityRow/activityRow.js
+++ src/components/activityRow/activityRow.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {View, Text, StyleSheet, Dimensions} from 'react-native';
-import {Avatar} from 'react-native-elements';
+import {Avatar, Divider} from 'react-native-elements';
 import {getActivityActorsString, getActionString} from '../../utils';
-import {fonts} from '../../constants';
+import {fonts, colors} from '../../constants';
 
 const ActivityRow = ({groupedAction}) => {
   const actorsString = getActivityActorsString(groupedAction);
   const actionString = getActionString(groupedAction);
-  const {users} = groupedAction;
-  const firstUserNames = Object.values(users)[0].name.split(' ');
+  const users = Object.values(groupedAction.users);
+  const firstUserNames = users[0].name.split(' ');
+
   const initials =
     firstUserNames.length > 1
       ? firstUserNames[0]
@@ -51,15 +52,36 @@ const ActivityRow = ({groupedAction}) => {
   return (
     <View style={styles.container}>
       {users.length > 1 ? (
-        <Avatar rounded icon={{name: 'group', type: 'font-awesome'}} />
+        <Avatar
+          rounded
+          icon={{name: 'group', type: 'font-awesome'}}
+          containerStyle={styles.avatar}
+        />
       ) : (
-        <Avatar rounded title={initials} titleStyle={styles.initials} />
+        <Avatar
+          rounded
+          title={initials}
+          titleStyle={styles.initials}
+          containerStyle={styles.avatar}
+        />
       )}
-      <View>
-        <Text>{actorsString}</Text>
-        <Text>{actionString}</Text>
+      <View style={styles.textContainer}>
+        <View>
+          <Text
+            numberOfLines={1}
+            ellipsizeMode="tail"
+            style={styles.actorsText}>
+            {actorsString}
+          </Text>
+          <Text
+            numberOfLines={1}
+            ellipsizeMode="tail"
+            style={styles.actionText}>
+            {actionString}
+          </Text>
+        </View>
+        <Text style={styles.timeDifferenceText}>{getTimeDifference()}</Text>
       </View>
-      <Text>{getTimeDifference()}</Text>
     </View>
   );
 };
@@ -68,11 +90,39 @@ const styles = StyleSheet.create({
   container: {
     width: Dimensions.get('window').width,
     flexDirection: 'row',
-    borderTopColor: '#EBF0F3',
     borderBottomColor: '#EBF0F3',
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    height: 48,
+  },
+  textContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: 11,
+    marginRight: 17,
+  },
+  actorsText: {
+    fontFamily: fonts.body_semi_bold,
+  },
+  actionText: {
+    paddingRight: 20,
+    fontFamily: fonts.body_regular,
+    color: colors.darkGrey,
+  },
+  avatar: {
+    marginLeft: 23,
   },
   initials: {
     fontFamily: fonts.body_regular,
+  },
+  timeDifferenceText: {
+    color: '#A6A6A6',
+    fontFamily: fonts.body_regular,
+    fontSize: 13,
+    position: 'absolute',
+    right: 0,
   },
 });
 
